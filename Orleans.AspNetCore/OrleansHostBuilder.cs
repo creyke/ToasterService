19b6f0d046c20a9a11b921a297b6a67fa8020bb8
Roger Creyke <rogerc@digitalgamingcorp.com>
2017-08-02 09:41:20 +0100

    Removed linux checks and added docker support.

--- Orleans.AspNetCore/OrleansHostBuilder.cs
+++ Orleans.AspNetCore/OrleansHostBuilder.cs
@@ -19,28 +19,11 @@ namespace Orleans.AspNetCore
             config.Defaults.Port = 11111;
             config.Globals.RegisterStorageProvider("Orleans.Storage.MemoryStorage", "Default");
             //config.Globals.RegisterBootstrapProvider("OrleansDashboard.Dashboard", "Dashboard");
-            if (IsLinux())
-            {
-                var hostName = Dns.GetHostName();
-                var ips = Dns.GetHostAddressesAsync(hostName).Result;
-                config.Defaults.HostNameOrIPAddress = hostName;
-                config.Globals.SeedNodes.Add(new IPEndPoint(ips.First(), 11111));
-                config.PrimaryNode = config.Globals.SeedNodes.First();
-            }
-            else
-            {
-                config.Defaults.HostNameOrIPAddress = "localhost";
-                config.Globals.SeedNodes.Add(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11111));
-                config.PrimaryNode = config.Globals.SeedNodes.First();
-            }
+            config.Defaults.HostNameOrIPAddress = "localhost";
+            config.Globals.SeedNodes.Add(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11111));
+            config.PrimaryNode = config.Globals.SeedNodes.First();
 
             return host;
-        }
-
-        private bool IsLinux()
-        {
-            // TODO: Improve. Stupid solution. Need to check if in container.
-            return RuntimeInformation.OSDescription.Contains("Linux");
         }
 
         public IOrleansHostBuilder UseStartup<TStartup>() where TStartup : class
